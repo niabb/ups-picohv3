4e40228d3ffaf0df7a93804a9f99b964c93d9545
Nicolas Abbal <nicolas.abbal@gmail.com>
2017-03-28 09:40:19 +0000

    First tests with I2C

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const i2c = require('i2c-bus');
+
+const STATUS_REGISTERS = 0x69;
+
+const STATUS_MODE = 0x00;
+const STATUS_BATLEVEL = 0x08;
+const STATUS_FV = 0x26;
+
+const i2c1 = i2c.openSync(1);
+
+
+function readStatus() {
+  let status = {};
+  status.poweringMode = i2c1.readByteSync(STATUS_REGISTERS, STATUS_MODE);
+  //status.batteryLevel = i2c1.readWordSync(STATUS_REGISTERS, STATUS_BATLEVEL).toString(16);
+  status.firmwareVersion = i2c1.readWordSync(STATUS_REGISTERS, STATUS_FV).toString(16);
+
+  console.log(status); 
+  return status;
+}
+
+readStatus();
