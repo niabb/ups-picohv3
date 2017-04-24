20f996a22eef1ce85c172c4f2ffaf0b834dad095
Nicolas Abbal <nicolas.abbal@gmail.com>
2017-04-24 17:11:18 +0200

    Added battery level.

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ const i2c1 = i2c.openSync(1);
 function readStatus() {
   let status = {};
   status.poweringMode = i2c1.readByteSync(STATUS_REGISTERS, STATUS_MODE);
-  //status.batteryLevel = i2c1.readWordSync(STATUS_REGISTERS, STATUS_BATLEVEL).toString(16);
+  status.batteryLevel = new Number(i2c1.readWordSync(STATUS_REGISTERS, STATUS_BATLEVEL).toString(16));
+  status.batteryLevel = status.batteryLevel/100;
   status.firmwareVersion = i2c1.readWordSync(STATUS_REGISTERS, STATUS_FV).toString(16);
 
   console.log(status); 
